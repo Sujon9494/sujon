45cd36fc75b16c7c0b378b7e0746274952184ddb
Sujon9494 <sujonhosen9466@gmail.com>
2018-09-10 20:05:08 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  //  echo "Hello World<br>";
+  //  print "Hello Mars";
+
+  //  $xyz ="Hello World";
+  //  echo $xyz;
+
+ // $name ="Rashed";
+  //echo "Welcome ".$name;
+
+
+ // $g ="Welcome ";
+ // $name ="Rashed";
+ // echo $g.$name;
+
+
+     $a =10;
+     $b =15;
+     $c =$a%$b;
+     echo $c;
+
+?>
---
+++ test.php
@@ -0,0 +1,63 @@
+<?php
+    // $age =20;
+    // if($age<18)
+    // {
+    //     echo "You are a child";
+    // }
+    // else
+    // {
+    //     echo "You are a young Man";
+    // }
+
+    // $s ="50";
+    // if($s===50)
+    // {
+    //     echo "equal";
+    // }
+    // else 
+    // {
+    //     echo "not equal";
+    // }
+
+
+    // $x =5;
+    // $y =10;
+    // if($x<=10){
+    //     if($y>=5){
+    //         echo "y is getat then";
+    //     }
+    //     else{
+    //         echo "y is less then";
+    //     }
+
+    // }
+    // else{
+    //     echo "x is getat then:;
+    // }
+
+    // $user="Admin";
+    // $pass ="123ABK";
+    // if(user === "Admin")
+    // {
+    //     if($pass===123ABK){
+    //         echo "Welcome";
+    //     }
+    //     else{
+    //         echo "wron";
+    //     }
+    // }
+    // else{
+    //     echo "Login fail"
+    // }
+
+        $student ="900";
+        $cocsiden ="true";
+        if($student <"900" || $cocsiden =="true")
+        {
+            echo "You are Prometd";
+        }
+        else{
+            echo "you are not prometd";
+        }
+
+?>
